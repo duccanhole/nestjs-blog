27a0d100c3f7bfa203ad22176e9f12addd75974f
duccanhole <duccanhole@gmail.com>
2022-10-08 22:49:11 +0700

    update process: validate data

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get, Req } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { IsString, MinLength } from 'class-validator';
 
+class createUserDto {
+  @IsString()
+  @MinLength(6)
+  userName;
+  @IsString()
+  @MinLength(6)
+  password;
+}
 
 @Controller('user')
-export class UserController{
-    @Get('info')
-    getUserInfo(){
-        return {
-            username: 'test',
-            posts: [],
-            postsSaved: []
-        }
-    }
+export class UserController {
+  @Get('info')
+  getUserInfo() {
+    return {
+      username: 'test',
+      posts: [],
+      postsSaved: [],
+    };
+  }
+  @Post('create')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  createUser(@Body() p: createUserDto) {
+    return {
+      statusCode: 200,
+      data: p,
+    };
+  }
 }
--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,8 +1,13 @@
+import { IsArray, IsString } from "class-validator";
 import { PostDto } from "./post.interface";
 
-export interface UserDto{
-    userName: String,
-    posted: PostDto[],
-    postSaved: PostDto[],
-    _id: String
+export class UserDto{
+    @IsString()
+    userName: String;
+    @IsArray()
+    posted: PostDto[];
+    @IsArray()
+    postSaved: PostDto[];
+    @IsString()
+    _id: String;
 }
